6513d31075e2e937211e366d077e86c57a87ab6c
diwpro007 <diwserala@gmail.com>
2018-05-22 14:44:15 +0545

    refactored

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+var putPoint = function(e) {
+    // context.arc();
+};
+$(document).ready(function() {
+    console.log("found");
+    var canvas = $('#canvas').get(0);
+    var context = canvas.getContext('2d');
+    canvas.width = 400;
+    canvas.height = 400;
+    canvas.addEventListener('mousedown', putPoint);
+});
